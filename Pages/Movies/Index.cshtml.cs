6b4e47178abaf1fc4ec55dda491fa22f7f1b4c89
WWL <819070864@qq.com>
2020-11-14 00:38:10 +0800

    StartPoint of part 7

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -13,6 +14,13 @@ namespace RazorPagesMovie.Pages.Movies
     public class IndexModel : PageModel
     {
         private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+        [BindProperty(SupportsGet =true)]
+        public string SearchString { get; set; }
+        [BindProperty(SupportsGet =true)]
+        public string MovieGenre { get; set; }
+
+        public SelectList Genres { get; set; }
+
 
         public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
         {
@@ -23,7 +31,21 @@ namespace RazorPagesMovie.Pages.Movies
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            if(!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(n => n.Title.Contains(SearchString));
+            }
+            Movie = await movies.ToListAsync();
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
         }
     }
 }
