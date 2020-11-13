8315baf48fa66b06f5a2de911ae0f2c8fb21d20a
WWL <819070864@qq.com>
2020-11-13 21:27:06 +0800

    Nullable page template

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -8,9 +9,10 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
-        [DataType(DataType.Date)]
+        [DataType(DataType.Date)][Display(Name ="Release Data")]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName ="decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
