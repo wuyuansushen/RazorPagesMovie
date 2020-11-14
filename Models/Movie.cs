dcd269d9e6919fb1e21d72cd3692c872f54a76aa
WWL <819070864@qq.com>
2020-11-14 14:03:49 +0800

    Server Validation

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,13 +7,27 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60,MinimumLength =3)]
+        [Required]
         public string Title { get; set; }
 
-        [DataType(DataType.Date)][Display(Name ="Release Data")]
+        [DataType(DataType.Date)]
+        [Display(Name ="Release Data")]
         public DateTime ReleaseDate { get; set; }
+        
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
         public string Genre { get; set; }
+        
         [Column(TypeName ="decimal(18, 2)")]
+        [Range(0,100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(5)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         public string Rating { get; set; }
     }
 }
